2c120867629f701187bc7fdd210f2922e726c0fb
Aahil <alwani.aahil25@gmail.com>
2022-12-05 03:30:38 +0500

    initial commit

--- MVC/Controller/SignUpController.js
+++ MVC/Controller/SignUpController.js
@@ -1,24 +1,44 @@
-class SignUpController {
-  calculateAge(dob) {
-    var mydob = new Date(dob);
-    //calculate month difference from current date in time  
-    var month_diff = Date.now() - mydob.getTime();  
-      
-    //convert the calculated difference in date format  
-    var age_dt = new Date(month_diff);   
-      
-    //extract year from date      
-    var year = age_dt.getUTCFullYear();  
-      
-    //now calculate the age of the user  
-    let age = Math.abs(year - 1970);  
-      
-    //display the calculated age  
-    return parseInt(age);
-  }
+import axios from "axios";
+const ip = require('../../API/NetworkConfig');
+
+export class SignUpController {
+
+    calculateAge(dob) {
+        var mydob = new Date(dob);
+        //calculate month difference from current date in time  
+        var month_diff = Date.now() - mydob.getTime();  
+        
+        //convert the calculated difference in date format  
+        var age_dt = new Date(month_diff);   
+        
+        //extract year from date      
+        var year = age_dt.getUTCFullYear();  
+        
+        //now calculate the age of the user  
+        let age = Math.abs(year - 1970);  
+        
+        //display the calculated age  
+        return parseInt(age);
+    }
+
+    async signup(user){
+        //axios post
+        let request= await axios.post(`http://${ip.address}/api/users`,{
+            cnic:user.cnic,
+            name:user.name,
+            dob:user.dob,
+            password:user.password,
+            gender:user.gender,
+        });
+
+        let response= request.data;
+
+        if(response){
+            return true;
+        }
+        return false;
+    }
 }
 
 //format:mm/dd/yyyy
 //console.log(new SignUpController().calculateAge('02/25/2001'));
-
-module.exports={SignUpController};
